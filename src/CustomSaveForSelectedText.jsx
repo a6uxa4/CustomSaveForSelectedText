0cfcb43feb568dedfe6bdceb53b20c1175fe823f
a6uxa4 <a6uxa4@icloud.com>
2023-02-12 23:23:36 +0600

    savem

--- src/CustomSaveForSelectedText.jsx
+++ src/CustomSaveForSelectedText.jsx
@@ -1,12 +1,22 @@
-import { useCallback, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
 export const useTectSelection = () => {
 	const [, update] = useState()
 	const forceUpdate = useCallback(() => update({}), [])
-	const [slection, setSelection] = useState(null)
+	const [selection, setSelection] = useState(null)
 
 	const handleSelection = () => {
 		setSelection(document.getSelection())
 		forceUpdate()
 	}
+
+	useEffect(() => {
+		setSelection(document.getSelection())
+		document.addEventListener('selectionchange', handleSelection)
+		return () => {
+			document.removeEventListener('selectionchange', handleSelection)
+		}
+	}, [])
+
+	return String(selection)
 }
